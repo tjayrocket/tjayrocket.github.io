26c010b6a462733ad233fb7172d38578bc9b750a
tjayrocket <tjayrocket@gmail.com>
2017-05-28 11:38:04 -0700

    Making it work

--- scripts/contentView.js
+++ scripts/contentView.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const contentView = {};
-
-contentView.initIndexPage = function() {
-  app.Content.all.forEach(function(content){
-    $('#content-block').append(content.toHtml())
-  });
-};
+// const contentView = {};
+//
+// contentView.initIndexPage = function() {
+//   app.Content.all.forEach(function(content){
+//     $('#content-block').append(content.toHtml())
+//   });
+// };
