b364be7cc4dca6147dbd85b4ac550d4b19c03cb1
tjayrocket <tjayrocket@gmail.com>
2017-06-09 10:43:18 -0700

    Wiped Clean

--- scripts/buttonView.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-$('#about-me').on('click', function(){
-  $('#content-block').hide();
-
-})
--- scripts/contentView.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// const contentView = {};
-//
-// contentView.initIndexPage = function() {
-//   app.Content.all.forEach(function(content){
-//     $('#content-block').append(content.toHtml())
-//   });
-// };
