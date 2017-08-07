29ad75fcf12ef15756b4ccec6adbcd028fd4dce6
T.Jay <tjayrocket@gmail.com>
2017-08-06 23:47:45 -0700

    Button Functionality moving ahead, lnking to resume, etc

--- app.js
+++ app.js
@@ -3,6 +3,15 @@
 $('#bottom-content-div').hide();
 
 $('#show').on('click', function(){
-  console.log('Yep');
   $('#bottom-content-div').fadeIn(500);
+});
+
+$('#about').on('click', function(){
+  console.log('about');
+});
+$('#developer').on('click', function(){
+  console.log('developer');
+});
+$('#music').on('click', function(){
+  console.log('musician');
 });
