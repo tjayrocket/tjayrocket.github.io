4fd2f11e77f7d98acbd1ef471b3b7aa24038bcf3
T.Jay Rocket <tjayrocket@gmail.com>
2017-08-12 14:42:36 -0700

    Testing Out React Deployment

--- README.md
+++
@@ -1,1 +0,0 @@
-# tjayrocket.github.io
---
+++ tjayrocket/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// --STYLES--
+import './styles/index.css';
+import './styles/App.css';
+import './styles/standard.css';
+import './styles/wide.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
